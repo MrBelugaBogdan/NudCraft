6a897d2e1a29d1e90ec9d10a2fd2b7f24b05538f
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-02 19:43:50 +0300

    Update init.lua

--- mods/custom_nodes/init.lua
+++ mods/custom_nodes/init.lua
@@ -5,3 +5,14 @@ minetest.register_node("custom_nodes:dark_brick", {
     groups = {cracky = 3},
     sounds = default.node_sound_stone_defaults(),
 })
+-- Другий блок: Армоване скло
+minetest.register_node("custom_nodes:reinforced_glass", {
+    description = "Reinforced Glass",
+    drawtype = "glasslike", -- Магія! Це робить блок прозорим
+    tiles = {"reinforced_glass.png"},
+    paramtype = "light",
+    sunlight_propagates = true, -- Світло проходить крізь нього
+    use_texture_alpha = "blend", 
+    groups = {cracky = 3, oddly_breakable_by_hand = 3},
+    sounds = default.node_sound_glass_defaults(),
+})
