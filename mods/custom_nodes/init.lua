d4a3fe7f376ead4b6b92ac5cd6f3f2a4aba8a436
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-02 19:57:24 +0300

    Add trampoline node with jumping functionality

--- mods/custom_nodes/init.lua
+++ mods/custom_nodes/init.lua
@@ -16,3 +16,21 @@ minetest.register_node("custom_nodes:reinforced_glass", {
     groups = {cracky = 3, oddly_breakable_by_hand = 3},
     sounds = default.node_sound_glass_defaults(),
 })
+-- 3. Стрибучий блок (Батут)
+minetest.register_node("custom_nodes:trampoline", {
+    description = "Super Trampoline",
+    tiles = {"trampoline.png"},
+    groups = {snappy = 3, choppy = 2}, -- Його легко зламати сокирою або мечем
+    sounds = default.node_sound_wood_defaults(),
+    
+    -- МАГІЯ СТРИБКА:
+    on_step = function(pos, player)
+        -- Перевіряємо, чи це гравець
+        if player:is_player() then
+            -- Отримуємо поточну швидкість гравця
+            local vel = player:get_velocity()
+            -- Даємо потужний стусан вгору (швидкість 12)
+            player:set_velocity({x = vel.x, y = 12, z = vel.z})
+        end
+    end,
+})
