ee6e719bb4bdd5fbb90a866f2234b99dc2609096
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-03-02 19:33:35 +0300

    Update init.lua

--- mods/custom_nodes/init.lua
+++ mods/custom_nodes/init.lua
@@ -1,1 +1,7 @@
-
+minetest.register_node("custom_nodes:dark_brick", {
+    description = "Dark Brick",
+    tiles = {"dark_brick.png"},
+    is_ground_content = true,
+    groups = {cracky = 3},
+    sounds = default.node_sound_stone_defaults(),
+})
